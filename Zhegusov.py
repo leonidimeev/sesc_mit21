6f9bf5fc439038ec7222d917737e021380323fb5
ZhoraPredator <gygychka2004@gmail.com>
2021-01-25 15:18:55 +0900

    PELMENY

---
+++ Zhegusov.py
@@ -0,0 +1,40 @@
+#print("Vnaturye!")
+#x = int(input("Vveditye pervoye ->"))
+#y = int(input("Vveditye pervoye ->"))
+#print("SUMMA", x+y)
+
+##x = int(input("VVEDITE CHOSLO -->"))
+#i = 0
+#while(i < 10):
+#    print(x)
+#    i += 1
+#print("end")
+
+#i = 0
+#while (i < 5):
+#    x = input("->")
+#    print(ord(x))
+#    i += 1
+
+
+
+#LABAROTORY WORK №2!
+
+#i = 0
+#x1 = 0
+#while (i < 5):
+#    x = int(input("->"))
+#    x = x + x1
+#    x1 = x
+#    i += 1
+#print(x)
+
+#i1 = int(input("Vvedite Kolixhestvo proizvedeniy"))
+#i = 0
+#x1 = 1
+#while (i < i1):
+#    x = int(input("->"))
+#    x = x * x1
+#    x1 = x
+#    i += 1
+#print(x)
