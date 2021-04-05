7a1a7fcf47c3b3df06b969748c6c4a7aac023677
Леонид Имеев <leonidimeev@yandex.ru>
2021-04-05 15:05:50 +0900

    added new exercises

--- student.py
+++ student.py
@@ -1,12 +1,9 @@
 class Student:
-    def __init__(self, name, age, group, hobbies):
+    def __init__(self, name, age, hobbie):
         self.name = name
         self.age = age
-        self.group = group
-        self.hobbies = hobbies
-    def getAllHobbies(self):
-        for hobbie in self.hobbies:
-            return(hobbie)
+        self.hobbie = hobbie
+        self.rank = 'двоечник'
 class Group:
     def __init__(self, name):
         self.name = name
@@ -15,14 +12,29 @@ class Group:
         self.students.append(student)
         print(student.name + ' added to group ' + self.name + '!')
     def printAllStudents(self):
+        print('Список студентов группы ' + group.name + ':')
         for student in self.students:
             print(student.name)
-
-group1 = Group('МИТ-10')
-student1 = Student('Кугунуров Андрей', 16, group1, ['компьютерные игры', 'кубик-кубика'])
-group1.addNewStudent(student1)
-print('Список студентов группы ' + group1.name + ':')
-group1.printAllStudents()
-for student in group1.students:
-    print(student.name + ' увлекается ', end='')
-    print(student.hobbies)
+    def printAllStudentsHobbie(self):
+        for student in self.students:
+            print(student.name + ' увлекается ', end='')
+            print(student.hobbies)
+    def printAllStudentWhoLikeThisHobbie(self, hobbie):
+        # TODO: создать функцию которая выводит на экран всех учеников с определенным хобби
+        pass
+    def printAllStudentWhoOlderThan16(self):
+        # TODO: создать функцию которая выводит на экран всех учеников старше 16
+        pass
+    def rankSet(self):
+        # TODO: создать функцию проставляющую всем ученикам в группе оценку
+        pass
+import random
+rank = ['двоечник', 'троечник', 'хорошист' , 'отличник', 'сын маминой подруги']
+names = ['Кугунуров Андрей','Рожков Денис','Санников Никита', 'Бысыев Дьулустаан', 'Соловьев Артем']
+hobbies = ['компьютерные игры', 'кубик-кубика', 'футбол', 'баскетбол', 'тетрис', 'настольный теннис']
+group = Group('МИТ-10')
+# TODO: сделать нормальную генерацию группы (добавить имена и чтоб в группу двух с одинаковыми именами не добавляло
+for i in range(10):
+    group.addNewStudent(Student(random.choice(names), random.randint(14,18), random.choice(hobbies)))
+group.printAllStudents()
+#group.printAllStudentsHobbie()
